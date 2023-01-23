de72a77eb4de270b776642cbb1d8e207d437c933
Ashik Ali <ashikali.m@gmail.com>
2023-01-23 11:01:22 +0530

    Fixed issues

---
+++ var/templates_c/c094d39b7b7ab4aab2c1ce38f283689b1c91177f_0.file.unlicensed.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.33, created on 2023-01-13 12:41:34
+  from '/var/www/html/modules/theme_designer/themes/default/unlicensed.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_63c10426df6469_40194853',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c094d39b7b7ab4aab2c1ce38f283689b1c91177f' => 
+    array (
+      0 => '/var/www/html/modules/theme_designer/themes/default/unlicensed.tpl',
+      1 => 1664271816,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63c10426df6469_40194853 (Smarty_Internal_Template $_smarty_tpl) {
+?><table width="99%" border="0" cellspacing="0" cellpadding="0" align="center">
+        <tr>
+            <td>
+            <?php echo $_smarty_tpl->tpl_vars['GURU_UNLICENSED']->value;?>
+
+            <td>
+        </tr>
+</table>
+
+
+<?php }
+}
